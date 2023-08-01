470dadd80b69bbadc9d5f01df518b3d0a73b6eb3
so4 <93477076+XuanPhat2008@users.noreply.github.com>
2023-08-01 12:12:13 +0700

    Update TimeLine.jsx
    ./img/ to ./assets/

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -11,7 +11,7 @@ const TimeLine = () => {
               <h3 className="date">1991</h3>
               <h1>WHAT ABOUT BOB?</h1>
               <div className="image-tl">
-                <img src="img/v2.png" alt="" />
+                <img src="assets/v2.png" alt="" />
               </div>
               <p>
                 Frank Oz's What About Bob? tells the story of Bob (Bill Murray) who has a massive list of phobias that interfere with his everyday life. His therapist Dr. Leo Marvin (Richard Dreyfuss) is certain that the answer to all of Bob's problems is his own best-selling book, "Baby Steps". Chaos ensues when Bob can't leave Dr. Marvin alone, even following him while the doctor tries to take a family vacation.
@@ -23,7 +23,7 @@ const TimeLine = () => {
               <h3 className="date">2001</h3>
               <h1>A BEAUTIFUL MIND</h1>
               <div className="image-tl">
-                <img src="img/v3.png" alt="" />
+                <img src="assets/v3.png" alt="" />
               </div>
               <p>
                 A Beautiful Mind is an inspiring biographical look at the life of John Nash (Russell Crowe). Nash was a mathematician who lived with schizophrenia. The movie acknowledges the many contributions Nash made to the field of mathematics while acknowledging the very real struggles he faced when his mental health was at its worst.
@@ -35,7 +35,7 @@ const TimeLine = () => {
               <h3 className="date">2011</h3>
               <h1>BOB'S BURGERs</h1>
               <div className="image-tl">
-                <img src="img/v1.png" alt="" />
+                <img src="assets/v1.png" alt="" />
               </div>
               <p>
                 If burger puns and fart jokes make you happy, you’ll fit right in with the Belchers. At Bob’s burger joint, there often aren’t enough butts in the seats, much to backside-obsessed eldest daughter Tina’s chagrin. But on this animated family series, there’s always a lot of love to go around.
@@ -47,7 +47,7 @@ const TimeLine = () => {
               <h3 className="date">2014-2022</h3>
               <h1>BLACK-ISH</h1>
               <div className="image-tl">
-                <img src="img/v4.png" alt="" />
+                <img src="assets/v4.png" alt="" />
               </div>
               <p>
                 Black-ish as a comedy never shied away from portraying serious issues. The show handled racism and sexism, and the COVID-19 pandemic was among the many real-world issues the show's characters encountered during the series' run. Tracee Ellis Ross received critical acclaim for her role as Dr. Rainbow Johnson.
@@ -60,7 +60,7 @@ const TimeLine = () => {
               <h3 className="date">I DON'T KNOW</h3>
               <h1>SEX EDUCATION</h1>
               <div className="image-tl">
-                <img src="img/v5.png" alt="" />
+                <img src="assets/v5.png" alt="" />
               </div>
               <p>
                 Sex Education features head boy Jackson Marchetti (Kedar Williams-Stirling), and his experiences at Moordale Secondary School. He's a championship swimmer and is on a quest to get Maeve (Emma Mackey) to date him. Even with all of his successes, he still lives with anxiety and panic attacks.
@@ -72,7 +72,7 @@ const TimeLine = () => {
               <h3 className="date">I DON'T KNOW</h3>
               <h1>MOON KNIGHT</h1>
               <div className="image-tl">
-                <img src="img/v6.png" alt="" />
+                <img src="assets/v6.png" alt="" />
               </div>
               <p> 
 Marvel's Moon Knight showcases a little-known Marvel hero, Marc Spector. Spector lives with Dissociative Identity Disorder (DID). This manifests as Marc having at least three alter-egos, that have different powers and abilities. He is the avatar of the Egyptian god Khonshu. Oscar Isaac plays each of Spector's alters.
