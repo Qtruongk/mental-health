3de33801f1b733f523e3f4ce76dca6225f5808e8
Qtruongk <Qtruongk@users.noreply.github.com>
2023-08-06 18:39:36 +0700

    update path 2

--- Main-Project/src/components/Pressure-blogs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Pressure=()=>{
-    return(
-        <div className="common-blogs">
-            <h1>Common</h1>
-        </div>
-    );
-};
-export default Pressure;
